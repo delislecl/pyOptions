35f02e9f6b288b80dbf810ba0cf2b822045912bb
Clement <delisle.cl@gmail.com>
2018-08-14 16:54:28 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyOptions',
+    version='0.1.0',
+    packages=['pyOptions'],
+    url='https://github.com/delislecl/pyOptions',
+    license='MIT',
+    author='Clement',
+    author_email='delisle.cl@gmail.com',
+    description='Options pricing package'
+)
